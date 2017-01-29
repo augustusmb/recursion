745b5d5b45f12635f0c9523bb50139dc3ffbf898
Augustus Brennan <augustusmb@gmail.com>
2017-01-29 14:11:29 -0800

    Complete Stringify and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+    var container = [];
+
+    var walk = function(element){
+      if (_.contains(element.classList, className)){
+          container.push(element);
+        }
+        _.each(element.childNodes, function(childNode){
+            walk(childNode);
+        })
+    };
+    walk(document.body);
+    return container;
 };
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,79 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+    if(typeof obj === "object" && obj !== null && !Array.isArray(obj)){
+        return stringifyObject(obj);
+    }
+
+    else if(Array.isArray(obj)){
+        return stringifyArray(obj);
+    }
+    else{
+      return stringifyImperatives(obj);
+    }
+
 };
+
+
+function stringifyImperatives(input) {
+  if (typeof input === 'string') {
+    return '"' + input + '"';
+  }
+  else if (input === null) {
+    return String(null);
+  } else {
+    return input.toString();
+  }
+}
+
+function stringifyArray(array) {
+  var container = [];
+
+  if (_.isEmpty(array)) {
+    return "[]";
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'object' && !Array.isArray(array[i]) && array[i] !== null) {
+      container.push(stringifyObject(array[i]));
+    }
+    else if (Array.isArray(array[i])) {
+      container.push(stringifyArray(array[i]));
+    }
+    else {
+      container.push(stringifyImperatives(array[i]));
+    }
+  }
+  return '[' + container.join() + ']';
+
+}
+
+function stringifyObject(obj) {
+
+  var container = [];
+
+  if(_.isEmpty(obj)){
+    return "{}";
+  }
+  for (var key in obj) {
+    if(typeof obj[key] === 'undefined' || typeof obj[key] === 'function'){
+      continue;
+    }
+    if (typeof obj[key] === 'object' && !Array.isArray(obj[key]) && obj[key] !== null) {
+      container.push(stringifyImperatives(key) + ':' + stringifyObject(obj[key]));
+    }
+    else if(Array.isArray(obj[key])){
+      container.push(stringifyImperatives(key) + ':' + stringifyArray(obj[key]));
+    }
+    else {
+      container.push(stringifyImperatives(key) + ':' + stringifyImperatives(obj[key]));
+    }
+  }
+
+  return '{' + container.join() + '}';
+}
+
+
+
+
